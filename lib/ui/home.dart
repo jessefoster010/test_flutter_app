2ff52856aef1c690f1215a18f1c8896b8d8f0efc
jesse <jessefoster010@gmail.com>
2020-05-10 15:01:23 -0500

    Starting movie details page

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -597,13 +597,99 @@ class MovieListViewDetails extends StatelessWidget {
         title: Text("Movies"),
         backgroundColor: Colors.blueGrey.shade900,
       ),
-      body: Center(
+      body: ListView(
+        children: <Widget>[
+          MovieDetailsThumbnail(
+            thumbnail: movie.images[0],
+          ),
+          MoviesDetailsHeaderWithPoster(
+            movie: movie,
+          )
+        ],
+      ),
+//      body: Center(
+//        child: Container(
+//          child: RaisedButton(
+//              child: Text("Go back ${this.movie.director}"),
+//              onPressed: () {
+//                Navigator.pop(context);
+//              }),
+//        ),
+//      ),
+    );
+  }
+}
+
+class MovieDetailsThumbnail extends StatelessWidget {
+  final String thumbnail;
+
+  const MovieDetailsThumbnail({Key key, this.thumbnail}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * .3,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(thumbnail), fit: BoxFit.cover)),
+            ),
+            Icon(
+              Icons.play_circle_outline,
+              size: 100,
+              color: Colors.white,
+            )
+          ],
+        ),
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0x00f5f5f5), Color(0xfff5f5f5)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter)),
+          height: 80,
+        )
+      ],
+    );
+  }
+}
+
+class MoviesDetailsHeaderWithPoster extends StatelessWidget {
+  final Movie movie;
+
+  const MoviesDetailsHeaderWithPoster({Key key, this.movie}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: <Widget>[MoviePoster(poster: movie.images[0].toString())],
+      ),
+    );
+  }
+}
+
+class MoviePoster extends StatelessWidget {
+  final String poster;
+
+  const MoviePoster({Key key, this.poster}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var borderRadius = BorderRadius.all(Radius.circular(10));
+    return Card(
+      child: ClipRRect(
+        borderRadius: borderRadius,
         child: Container(
-          child: RaisedButton(
-              child: Text("Go back ${this.movie.director}"),
-              onPressed: () {
-                Navigator.pop(context);
-              }),
+          width: MediaQuery.of(context).size.width / 4,
+          height: 160,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(poster), fit: BoxFit.cover)),
         ),
       ),
     );
